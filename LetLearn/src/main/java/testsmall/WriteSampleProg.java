d02f62ec98a6593d07dd90b450cc73e4083a09a8
Rajam <Rajam@192.168.1.104>
2023-09-13 23:22:02 +0530

    Added a package Testsmall

---
+++ LetLearn/src/main/java/testsmall/WriteSampleProg.java
@@ -0,0 +1,19 @@
+package testsmall;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WriteSampleProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, null);
+		driver.quit();
+
+	}
+
+}
